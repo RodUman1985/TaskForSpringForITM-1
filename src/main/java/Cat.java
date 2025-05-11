0b5d272210db6af7fa88ca0de63cab203440d6f4
RodUman1985 <Vand_85@mail.ru>
2025-05-11 13:45:35 +0300

    1

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String name;
+
+    public String getName() {
+        return "это кот с именем "+ name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
